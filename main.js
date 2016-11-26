c4a5f5a0df0161e994d1483c30698dbfd007a1fc
Wibren Van Assche <r0368680@student.thomasmore.be>
2016-11-26 19:30:01 +0100

    ex3 deposit en withdraw met validatie en ease of use

---
+++ main.js
@@ -0,0 +1,64 @@
+var accounts = [
+    {name: 'Wibren', balance: 40},
+    {name: 'roro', balance: 0}
+];
+//***********************************create account************************************************
+function createaccount (naam, bedrag){
+    var account= {};
+    account.name= naam;
+    account.balance=bedrag;
+    accounts.push(account);
+};
+//***************************************deposit***************************************************
+function deposit (naam, bedrag){
+    if ( typeof bedrag === 'number'){
+        for (i =0; i<accounts.length; i++){
+            if (accounts[i].name === naam){
+                accounts[i].balance += bedrag;
+                break
+            }
+        }
+    }
+    else {
+        console.log("geef een geldig nummer")
+    }
+}
+//voorbeeld dat op toledo staat doet volgens mij niet wat het moet doen. deposit werkt en valideert
+
+//test deposit
+//deposit('Wibren',20);
+//deposit(Wibren','m')
+
+
+//****************************************withdraw******************************************************
+function withdraw (naam, bedrag){
+    if ( typeof bedrag === 'number'){
+        for (i =0; i<accounts.length; i++){
+            if (accounts[i].name === naam){
+                if(accounts[i].balance > bedrag){
+                accounts[i].balance -= bedrag;
+                }
+                else{
+                    console.log("ontoereikend saldo")
+                }
+                break
+            }
+            
+        }
+    }
+    else {
+        console.log("geef een geldig nummer")
+    }
+}
+//voorbeeld dat op toledo staat doet volgens mij niet wat het moet doen. deposit werkt en valideert
+
+//test withdraw
+/*
+withdraw('Wibren',20);
+withdraw('roro','m');
+withdraw('roro',5);
+ */
+
+
+console.log(accounts);
+
